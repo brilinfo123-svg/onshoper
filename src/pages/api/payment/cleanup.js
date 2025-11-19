2f3204164a442c4f76409da20306141350d54a75
brilinfo123-svg <brilinfo123@gmail.com>
2025-11-20 02:53:06 +0530

    ssa

--- src/pages/api/payment/cleanup.js
+++
@@ -1,7 +0,0 @@
-// /pages/api/cleanup.js
-import { cleanupExpiredCategories } from "@/utils/cleanupExpiredCategories";
-
-export default async function handler(req, res) {
-  await cleanupExpiredCategories();
-  res.status(200).json({ success: true, message: "Expired categories cleaned up" });
-}
