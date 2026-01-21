ab0612991515a0b976e0deeee613e4c18c1a1314
brilinfo123-svg <brilinfo123@gmail.com>
2026-01-21 21:33:52 +0530

    Worked on ui

--- src/pages/_offline.tsx
+++ src/pages/_offline.tsx
@@ -1,25 +1,33 @@
-import styles from "@/styles/offline.module.scss";
-import Image from "next/image";
-
 export default function Offline() {
-    return (
-      <div className={styles.offlineWrapper}>
-        <Image
-        src="/images/wifi.png"   // 👈 apna custom image rakho public/images folder me
-        alt="No Internet"
-        width={130}
-        height={130}
-        className={styles.offlineImage}
-      />
+  return (
+    <>
+      <link rel="stylesheet" href="/offline.css" />
+
+      <div className="offlineWrapper">
+        <img
+          src="/images/wifi.png"
+          alt="No Internet"
+          width={130}
+          height={130}
+          className="offlineImage"
+        />
+
         <h1>No Internet Connection</h1>
         <p>Please check your network and try again.</p>
-        <button className={styles.retryButton} onClick={() => {if (navigator.onLine) {window.location.href = "/";
-          } else {
-            window.location.reload();
-          }}}>Refresh Page
-        </button>
 
+        <button
+          className="retryButton"
+          onClick={() => {
+            if (navigator.onLine) {
+              window.location.href = "/";
+            } else {
+              window.location.reload();
+            }
+          }}
+        >
+          Refresh Page
+        </button>
       </div>
-    );
-  }
-  
+    </>
+  );
+}
