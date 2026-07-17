d2f8ef1ad6c364864a1dec0b891a5398b06fef3e
kkashy <kshemender.kashyap@truinc.ca>
2026-07-17 21:23:05 +0530

    worked on messagenger

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -1,7 +1,7 @@
 // pages/chat/index.jsx
 "use client";
 
-import ChatLayout from "@/components/Chat/ChatLayout/index";
+import ChatLayout from "@/components/Chat/ChatLayout/Index";
 
 export default function ChatPage(){
 return(
